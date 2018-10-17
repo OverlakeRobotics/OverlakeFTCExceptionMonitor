c9ea1126cc13fc168c49750f994bf5213d4598dd
unholydeath785 <ivansonofcoul@gmail.com>
2018-10-16 17:20:34 -0700

    initial commit

---
+++ app/src/main/java/com/overlake/ftc/ftcexceptionmoniter/ExceptionMoniter.java
@@ -0,0 +1,15 @@
+package com.overlake.ftc.ftcexceptionmoniter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExceptionMoniter extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exception_moniter);
+    }
+}
